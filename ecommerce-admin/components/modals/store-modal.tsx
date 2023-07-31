dff7de4da05f47e0da27f55de6a31d92c3688b51
Hamza <337118@student.mboutrecht.nl>
2023-07-31 18:38:08 +0200

    navbar-semi done and store switcher

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -41,7 +41,9 @@ export const StoreModal = () => {
 
             const response = await axios.post("/api/stores", values);
 
-            toast.success("Store created successfully.")
+            // toast.success("Store created successfully.")
+
+            window.location.assign(`/${response.data.id}`);
         } catch (error) {
             // console.log(error);
             toast.error("Something went wrong.");
