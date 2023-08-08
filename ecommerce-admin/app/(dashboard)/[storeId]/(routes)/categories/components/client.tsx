1a50369c3d73452a82e6adeaf1cfe221a63bae08
Hamza <337118@student.mboutrecht.nl>
2023-08-08 18:47:41 +0200

    categories,billboards crud done

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -9,16 +9,16 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-import { BillboardColumn, columns } from "./columns"
+import { CategoryColumn, columns } from "./columns"
 import { ApiList } from "@/components/ui/api-list";
 
 
-interface BillboardClientProps {
-    data: BillboardColumn[]
+interface CategoryClientProps {
+    data: CategoryColumn[]
 }
 
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const CategoryClient: React.FC<CategoryClientProps> = ({
     data
 }) => {
     const router = useRouter();
@@ -30,19 +30,19 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage your billboards for your store."
+                    title={`Categories (${data.length})`}
+                    description="Manage categories for your store."
                 />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
-            <DataTable columns={columns} data={data}  searchKey="label"/>
-            <Heading title="API" description="API calls for Billboards"/>
+            <DataTable columns={columns} data={data} searchKey="name" />
+            <Heading title="API" description="API calls for categories" />
             <Separator />
-            <ApiList  entityName="billboards" entityIdName="billboardId"/>
+            <ApiList entityName="categories" entityIdName="categoryId" />
         </>
     )
 }
--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -2,28 +2,32 @@ import {format} from "date-fns"
 
 
 import prismadb from "@/lib/prismadb"
-import { BillboardClient } from "./components/client"
-import { BillboardColumn } from "./components/columns"
+import { CategoryClient } from "./components/client"
+import { CategoryColumn } from "./components/columns"
 
-const BillboardsPage = async ({
+const CategoriesPage = async ({
     params
 }: {
     params: {
         storeId: string
     }
 }) => {
-    const billboards = await prismadb.billboard.findMany({
+    const categories = await prismadb.category.findMany({
         where: {
             storeId: params.storeId
+        },
+        include : {
+            billboard : true,
         },
         orderBy: {
             createdAt: "desc"
         }
     });
 
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    const formattedCategories: CategoryColumn[] = categories.map((item) => ({
         id: item.id,
-        label: item.label,
+        name: item.name,
+        billboardLabel : item.billboard.label,
         createdAt: format(item.createdAt, "MMMM dd, yyyy")
 
     }));
@@ -32,10 +36,10 @@ const BillboardsPage = async ({
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-5 p-8 pt-6">
-                <BillboardClient data={formattedBillboards} />
+                <CategoryClient data={formattedCategories} />
             </div>
         </div>
     )
 }
 
-export default BillboardsPage
+export default CategoriesPage
