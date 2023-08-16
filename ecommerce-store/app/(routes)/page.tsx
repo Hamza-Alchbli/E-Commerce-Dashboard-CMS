82e4c3a3d991fb8d294572baa67aba03962fae8b
Hamza <337118@student.mboutrecht.nl>
2023-08-16 20:37:14 +0200

    billboard read in homepage

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+
+const HomePage = async () => {
+    const billboard = await getBillboard("afbb72ef-fe5e-458b-8c09-f3b3591e39b2");
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+     <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+     </Container>
     )
 }
 
