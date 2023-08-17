e2d25e379235d112b1ed13440b9569c83765315c
Hamza <337118@student.mboutrecht.nl>
2023-08-17 17:25:41 +0200

    featured items and single product page

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -11,14 +11,14 @@ const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("afbb72ef-fe5e-458b-8c09-f3b3591e39b2");
     return (
-     <Container>
+        <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={billboard}/>
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
             </div>
-            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                <ProductList title="Featured Products" items={products} />
-            </div>
-     </Container>
+        </Container>
     )
 }
 
